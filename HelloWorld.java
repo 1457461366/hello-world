33f4c1249696db2421b7523b2c95e864dd3afd54
Tony <1457461366@qq.com>
2019-02-27 19:09:45 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package Users.tony.Desktop;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			System.out.println("我爱你，我的小宝贝，你也会爱我的吧");
+		}
+	}
+}
+ 
